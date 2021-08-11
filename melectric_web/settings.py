2354b8b6face0681e71caa63233eb331208173fe
Paul Cabeza <ovidio.cabeza@gmail.com>
2021-08-11 15:54:24 -0600

    updated mail credentials

--- melectric_web/settings.py
+++ melectric_web/settings.py
@@ -135,6 +135,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'villo.cabeza@gmail.com'
-EMAIL_HOST_PASSWORD = 'villO7164'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = False
