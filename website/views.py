4742e4409a27c1fe5ed1bf055094d0812fb60e0b
Paul Cabeza <ovidio.cabeza@gmail.com>
2021-08-16 07:00:41 -0600

    updated links on index and adding about page

--- website/views.py
+++ website/views.py
@@ -6,6 +6,9 @@ from django.core.mail import send_mail
 def home(request):
 	return render(request, 'home.html', {})
 
+def about(request):
+	return render(request, 'about.html', {})
+
 def contact(request):
 	if request.method == 'POST':
 		message_f_name = request.POST['f_name']
